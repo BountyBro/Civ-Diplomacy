01ad0705bf633984780ccfe8d545cff83a29868c
BountyBro <32472955+BountyBro@users.noreply.github.com>
2025-05-22 13:59:20 -0700

    visualize.py added

---
+++ visualize.py
@@ -0,0 +1,10 @@
+# The visualization module for the civ diplomacy project. Contains functions to display the simulation and plot data.
+import matplotlib as plt
+
+
+
+def visualize(data):
+  pass # TO-DO: Provides an animated visualization of the simulation utilizing matplotlib
+
+def winPlot(data):
+  pass # TO-DO: Displays a graph (form TBD) portraying the prevalence in victory between cooperative and aggressive civs.
