a9a84a4a84b098473b097eff3813464d83acb003
BountyBro <32472955+BountyBro@users.noreply.github.com>
2025-05-22 13:48:38 -0700

    civ.py reformatted

--- civ.py
+++ civ.py
@@ -1,6 +1,8 @@
-# The civilization agent file.
+# The civilization agent file for tracking a civilization's
 
-class civ(self, tech, culture, military):
-  self.tech = tech
-  self.culture = culture
-  self.military = military
+class civ:
+  def __init__(self, tech, culture, military):
+    self.tech = tech
+    self.culture = culture
+    self.military = military
+    self.num_planets = 0
