af9cd8efea2d56270b88a12f423761b18a8fb6b7
BountyBro <32472955+BountyBro@users.noreply.github.com>
2025-05-22 13:47:31 -0700

    planet.py added

---
+++ planet.py
@@ -0,0 +1,5 @@
+# The file containing the planet.py agent, acting as mediums for interaction between civs and the form of placing civs across the model grid.
+
+class planet:
+  def __init__(loc):
+    self.loc = loc
