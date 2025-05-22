700c63b294f28bf442bcf6dcd79ed5b2a469df98
BountyBro <32472955+BountyBro@users.noreply.github.com>
2025-05-22 14:01:41 -0700

    civ.py updated

--- civ.py
+++ civ.py
@@ -1,8 +1,9 @@
 # The civilization agent file for tracking a civilization's
 
 class civ:
-  def __init__(self, tech, culture, military):
+  def __init__(self, tech, culture, military, friendly):
     self.tech = tech
     self.culture = culture
     self.military = military
-    self.num_planets = 0
+    self.friendly = friendly  # Boolean integer value (i.e. 0 or 1). 0 marking aggressive civs, 1 marking cooperative civs.
+    self.num_planets = 0      # Positive integer value. civ loses at self.num_planets == 0.
