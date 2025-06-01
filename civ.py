d6d501bed21ad7b7899a90df98a20dedb21842f3
Daniel <danielmorozov58@gmail.com>
2025-05-31 22:24:21 -0700

    Civ update_attribute update

--- civ.py
+++ civ.py
@@ -11,6 +11,11 @@ from random import random
 
 ##### CONSTANTS #####
 MAX_CULTURE = 100   # Amount of culture required for a culture victory.
+beta_P = 0.5   # Tech growth from population
+beta_E = 0.3   # Tech growth from energy
+delta_P = 0.0005  # Culture growth per population
+delta_T = 0.001   # Culture growth per tech
+delta_R = 0.0005  # Culture growth per resource unit
 
 
 
@@ -46,24 +51,62 @@ class Civ:
         self.max_growth_rate = 0                # Ceiling of population growth.
         
         
-    def update_attributes(self, culture_step= 0, friendliness_step= 0, military_step = 0, resources_step= [0] * 3):
-        # Population
-        self.population += self.population * self.max_growth_rate * min(1.0, float(self.resources[1]) / self.population)
-        # Attributes
-        self.culture += culture_step
-        self.friendliness += friendliness_step
-        self.military += military_step
-        self.tech += 0.5 * np.log(self.population) + 0.3 * self.resources[0] / self.population
-        # Resources
-        self.resources += resources_step
-        self.demand = np.array([(self.population + self.tech + self.military) / 10, self.population, self.military * 0.3])
+    def update_attributes(self, culture_step= 0, friendliness_step= 0, military_step = 0, resources_step= [0] * 3):\
+    
+        food_per_capita = self.resources[1] / max(self.population, 1e-3)
+        growth_rate = self.max_growth_rate * min(1.0, food_per_capita)
+        self.population += self.population * growth_rate
+        
+        # Resources update
+        for i in range(len(self.resources)):
+            self.resources[i] += resources_step[i]
+
+        # Tech Update
+        energy = max(self.resources[0], 1e-3)  # prevent division by zero
+        self.tech += beta_P * np.log(self.population) + beta_E * (energy / self.population)
+
+        # Culture Update
+        total_resources = sum(self.resources)
+        self.culture += delta_P * self.population + delta_T * self.tech + delta_R * total_resources
+
+        # Military Update
+        sigma_P = 0.1
+        sigma_T = 0.2
+        sigma_M = 0.3
+        minerals = self.resources[2]
+        self.military = sigma_P * self.population + sigma_T * self.tech + sigma_M * minerals
+
+        # Friendliness Updatetheta = 0.15
+        beta_f = 0.1
+        victories = getattr(self, 'victories', 0)  # default 0 if not set
+        friendliness_after_victories = max(0.0, self.friendliness - theta * victories)
+        cultural_pacification = beta_f * (self.culture / MAX_CULTURE)
+        self.friendliness = min(1.0, friendliness_after_victories + cultural_pacification)  # cap friendliness at 1
+
+        # Demand, Surplus and Deficit Update
+
+
+        f_c = 1.0  # Food consumption per capita (adjust if necessary)
+        e_c = 1.0  # Energy consumption per capita
+        alpha_T = 0.1
+        alpha_M = 0.1
+        m_c = 0.3
+
+        demand_energy = e_c * self.population + alpha_T * self.tech + alpha_M * self.military
+        demand_food = f_c * self.population
+        demand_minerals = m_c * self.military
+
+        self.demand = np.array([demand_energy, demand_food, demand_minerals])
+
+        # Calculate surplus and deficit
         flux = self.resources - self.demand
-        self.surplus = np.where(0 < flux, flux, 0)
-        self.deficit = np.where(flux < 0, 0, flux)
-        # Check Victory Condition
+        self.surplus = np.where(flux > 0, flux, 0)
+        self.deficit = np.where(flux < 0, -flux, 0)  # deficits are positive quantities
+
+        # Check culture victory condition
         if not self.has_won_culture_victory and self.culture >= MAX_CULTURE:
-            self.has_won_culture_victory = True # Set flag
-            proclaim_culture_victory(self.civ_id) # Call local version
+            self.has_won_culture_victory = True
+            proclaim_culture_victory(self.civ_id)
 
     def check_if_dead(self, t):
         if self.num_planets == 0:
